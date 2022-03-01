17a7703b5a598201d04d6b7e45fd81b71c27abcf
aps74 <darti64@hotmail.com>
2022-03-02 00:59:07 +0100

    Added force option to download

--- librairies/common/package_manager.py
+++ librairies/common/package_manager.py
@@ -220,7 +220,7 @@ class PackageManager(object):
             if package.complete or force:
                 build_targets = package.get_build_targets()
                 for build_target in build_targets:
-                    if build_target.build.complete:
+                    if build_target.build is not None and build_target.build.complete:
                         if not already_downloaded_build_targets.__contains__(build_target.name):
                             # store the data necessary for the next steps
                             build_os_path = f"{self.builds_path}/{build_target.name}"
